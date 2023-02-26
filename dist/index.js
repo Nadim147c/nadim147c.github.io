a4165ceb7fdb56f0d6b61b0cde7a42c87c663ab9
Ephemeral <90786729+Nadim147c@users.noreply.github.com>
2023-02-26 22:59:01 +0600

    Feat: add mouse trailer and typescript Added mouse trailer which increase it's size based on mouse move velocity and added typescript.

---
+++ dist/index.js
@@ -0,0 +1,89 @@
+const title = document.querySelector(".title");
+const username = document.querySelector(".username");
+const links = document.querySelector(".links");
+const trailer = document.querySelector(".trailer");
+const styleSheet = document.styleSheets[0];
+const defaultTitle = title.innerText;
+const defaultUsername = username.innerText;
+const mobile = /Mobi/i.test(navigator.userAgent);
+let mouseHover = false;
+let lastMoveTime = formatTime();
+linksData.forEach((link, index) => {
+    const image = document.createElement("img");
+    image.setAttribute("src", `icons/${link.icon}`);
+    image.setAttribute("alt", `${link.username} on ${link.service}`);
+    image.className = "icon";
+    if (!mobile) {
+        image.onmouseenter = () => {
+            // for making trailer to redirect arrow on hover
+            mouseHover = true;
+            trailer.classList.add("trailer-link-hover");
+            // Changing bg text
+            setText(title, link.service);
+            setText(username, `@${link.username}`);
+            // Hiding other elements that aren't hovered
+            const links = document.querySelectorAll(".icon");
+            links.forEach((link) => {
+                if (link.getAttribute("index") !== index.toString())
+                    link.classList.add("hidden");
+            });
+        };
+        image.onmouseleave = () => {
+            // for reset trailer to normal after hover
+            mouseHover = false;
+            trailer.classList.remove("trailer-link-hover");
+            // Resetting bg text
+            setText(title, defaultTitle);
+            setText(username, defaultUsername);
+            // Showing elements after hover is ended
+            const links = document.querySelectorAll(".icon");
+            links.forEach((link) => link.classList.remove("hidden"));
+        };
+    }
+    const anchor = document.createElement("a");
+    anchor.setAttribute("href", link.url);
+    anchor.className = "icon-container";
+    anchor.setAttribute("target", "_blank");
+    anchor.setAttribute("index", index.toString());
+    anchor.appendChild(image);
+    links.appendChild(anchor);
+});
+document.onmousemove = (e) => {
+    // current mouse position
+    const x = e.clientX - 15;
+    const y = e.clientY - 15;
+    // current trailer position
+    const lx = trailer.offsetLeft;
+    const ly = trailer.offsetTop;
+    // Distance between mouse and trailer
+    const distance = Math.sqrt((x - lx) ** 2 + (y - ly) ** 2);
+    // Getting time of move movement
+    const currentTime = formatTime();
+    const time = currentTime - lastMoveTime;
+    lastMoveTime = currentTime;
+    // Converting velocity to scale
+    const velocity = Math.min(distance / (time * 3), 3);
+    const scale = parseFloat(velocity.toFixed(2));
+    const left = `${x}px`;
+    const top = `${y}px`;
+    // Getting middle point between mouse position and trailer position
+    const [xm, ym] = getMiddlePoint(x, y, lx, ly);
+    const backgroundColor = mouseHover ? "#fff4" : "#fff";
+    const keyframes = [
+        {},
+        {
+            scale: mouseHover ? "3" : scale.toString(),
+            backgroundColor,
+            top: ym,
+            left: xm,
+        },
+        {
+            scale: mouseHover ? "3" : "0",
+            backgroundColor,
+            top,
+            left,
+        },
+    ];
+    trailer.animate(keyframes, { duration: 300, fill: "forwards" });
+};
+//# sourceMappingURL=index.js.map
